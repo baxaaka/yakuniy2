94194c1bd013aa7043e36105c52c497af8781cc8
baxaaka <jockuhacker@gmail.com>
2023-10-31 22:40:23 +0500

    okk

---
+++ app.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("input");
+const modal = document.querySelector(".modal");
+
+const button = document.querySelector("button");
+
+
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode == 13) {
+    if (input.value.toLocaleLowerCase().trim() == "mrx707") {
+      modal.style.display = "none";
+
+    }else alert("xato")
+  }
+});
